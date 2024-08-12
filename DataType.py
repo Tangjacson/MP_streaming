4791244bf8098340b15cb424fb04eee91c618695
jiaxiang tang <jiaxiangtang@infra04-wg324.eduroam.wireless.umn.edu>
2024-08-11 20:26:21 -0500

    a

---
+++ DataType.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class Path:
+    def __init__(self, id):
+        self.id = id
+
+    def setup(self,rtt):
+        self.rtt = 0
+        self.stream_list = []
+    
+    def close(self):
+        self.rtt = 9999
+
+    def assign_stream(self, stream):
+        self.stream_list.append(stream)
+
+
+
+class stream:
+    def __init__(self, id, time_stamp, length):
+        self.id = id
+        self.time_stamp = time_stamp
+        self.length = length
+        self.bit_rate
+        self.msssim
+        self.psnr
